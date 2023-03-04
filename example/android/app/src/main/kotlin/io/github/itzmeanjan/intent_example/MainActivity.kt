b594fe19676ea11d8b5466ee810431c3d86b444a
oovvoo_ <geek@secretcode.kr>
2023-03-04 21:26:34 +0900

    Initial commit

---
+++ example/android/app/src/main/kotlin/io/github/itzmeanjan/intent_example/MainActivity.kt
@@ -0,0 +1,23 @@
+package io.github.itzmeanjan.intent_example
+
+import io.flutter.embedding.android.FlutterActivity;
+
+
+class MainActivity: FlutterActivity() {
+
+//  override fun onCreate(savedInstanceState: Bundle?) {
+//    super.onCreate(savedInstanceState)
+//    GeneratedPluginRegistrant.registerWith(this)
+//  }
+//private val CHANNEL = "samples.flutter.dev/battery"
+     /*   @Override
+        public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+                GeneratedPluginRegistrant.registerWith(flutterEngine);
+                new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+                        .setMethodCallHandler(
+                                    (call, result) -> {
+                                    // Your existing code
+                            }
+                );
+            }*/
+}
